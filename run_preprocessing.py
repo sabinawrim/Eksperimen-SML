671a419c9276e8126aa3d29f7660d1c3ec6a4c63
sabinawrim <sabinawardaniah@gmail.com>
2025-10-29 10:22:09 +0700

    install modul

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -7,14 +7,15 @@ def main():
     raw_path = os.path.join(base_dir, "diabetes.csv")   
     output_dir = os.path.join(base_dir, "preprocessing/pima_diabetes_processed")
 
-    # Load data
+    # load data
     df = pd.read_csv(raw_path)
 
-    # Jalankan preprocessing
+    # jalankan preprocessing
     X_train, X_test, y_train, y_test = preprocess_data(df)
 
-    # Simpan hasil
+    # simpan hasil
     save_preprocessed_data(X_train, X_test, y_train, y_test, output_dir)
 
 if __name__ == "__main__":
     main()
+
